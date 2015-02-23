970d29cfe375fe0d95b3183eae5296f9c8d8c1ac
Zack Mullaly <redwire@riseup.net>
2015-02-23 20:15:25 -0330

    Implemented a simple side nav

--- site/public/javascripts/main.js
+++ site/public/javascripts/main.js
@@ -1,3 +1,12 @@
 (function () {
+  var overlay = document.getElementById('overlay');
+  overlay.style.display = 'none';
 
+  overlay.addEventListener('click', function (evt) {
+    overlay.style.display = 'none';
+  });
+
+  document.getElementById('hamburger').addEventListener('click', function (evt) {
+    overlay.style.display = '';
+  });
 })();
