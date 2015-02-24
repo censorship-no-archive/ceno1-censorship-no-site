d00aef04cb246d615430ae1f2f65700f811f2d0b
Zack Mullaly <redwire@riseup.net>
2015-02-23 21:16:07 -0330

    Add ability to serve different-language pages

--- site/routes/index.js
+++ site/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.render('en/index', { title: 'CeNo!' });
+});
+
+router.get('/:language', function (req, res) {
+  var language = req.params.language;
+  res.render(language + '/index', { title: 'CeNo!' });
 });
 
 module.exports = router;
