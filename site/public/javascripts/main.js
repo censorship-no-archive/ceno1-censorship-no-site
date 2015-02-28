7a5db5b317297bde7fd468f734b045282b575c1c
Zack Mullaly <redwire@riseup.net>
2015-02-27 21:58:08 -0330

    Basic implementation of Scott's design

--- site/public/javascripts/main.js
+++ site/public/javascripts/main.js
@@ -1,13 +1,3 @@
 (function () {
-  var overlay = document.getElementById('overlay');
-  overlay.style.display = 'none';
 
-  overlay.addEventListener('click', function (evt) {
-    overlay.style.display = 'none';
-  });
-
-  document.getElementById('hamburger').addEventListener('click', function (evt) {
-    overlay.style.display = '';
-    evt.preventDefault();
-  });
 })();
